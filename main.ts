ded62838757c8a727368b9ef7f063de2fd8eacde
Michle99 <sammiyonyx@gmail.com>
2025-01-02 10:31:15 -0500

    initial build

--- main.ts
+++ main.ts
@@ -1,89 +1,98 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {
+	App,
+	Plugin,
+	PluginSettingTab,
+	Setting,
+	Notice,
+} from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface NoteProgressSettings {
+	streakCount: number;
+	milestoneCount: number;
+	badgeCount: number;
+	lastNoteDate: string; // The date the last note was taken.
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+const DEFAULT_SETTINGS: NoteProgressSettings = {
+	streakCount: 0,
+	milestoneCount: 0,
+	badgeCount: 0,
+	lastNoteDate: "",
+};
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class NoteProgressTrackerPlugin extends Plugin {
+	settings: NoteProgressSettings;
 
 	async onload() {
+		console.log('Note Progress Tracker plugin loaded');
+		this.settings = Object.assign({}, DEFAULT_SETTINGS);
+
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
+		id: 'track-note',
+		name: 'Track Note Progress',
+		callback: async () => {
+			const currentDate = new Date().toISOString().split('T')[0];
+			const lastNoteDate = this.settings.lastNoteDate;
+
+			// Check if note was taken on the current day to update streak
+			if (lastNoteDate !== currentDate) {
+			this.updateStreak();
+			this.settings.lastNoteDate = currentDate;
 			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
+			await this.saveSettings();
+			this.showMilestonesAndBadges();
+		},
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.addSettingTab(new NoteProgressSettingsTab(this.app, this));
 	}
 
-	onunload() {
+	async updateStreak() {
+		const currentDate = new Date().toISOString().split('T')[0];
+		const lastNoteDate = this.settings.lastNoteDate;
 
+		if (lastNoteDate) {
+		const lastDate = new Date(lastNoteDate);
+		const diff = (new Date(currentDate).getTime() - lastDate.getTime()) / (1000 * 3600 * 24);
+
+		if (diff === 1) {
+			this.settings.streakCount++;
+		} else if (diff > 1) {
+			this.settings.streakCount = 1; // Reset streak if more than 1 day gap.
+		}
+		} else {
+		this.settings.streakCount = 1; // First streak
+		}
+
+		this.checkMilestonesAndBadges();
+	}
+
+	checkMilestonesAndBadges() {
+		if (this.settings.streakCount === 7) {
+		this.settings.badgeCount++; // Award a "1 week streak" badge.
+		new Notice('Congratulations! You earned the 1 Week Streak Badge!');
+		}
+
+		if (this.settings.streakCount === 30) {
+		this.settings.badgeCount++; // Award a "1 month streak" badge.
+		new Notice('Congratulations! You earned the 1 Month Streak Badge!');
+		}
+	}
+
+	showMilestonesAndBadges() {
+		new Notice(`Current Streak: ${this.settings.streakCount} days.`);
+		new Notice(`Total Badges: ${this.settings.badgeCount}`);
+		new Notice(`Milestones Completed: ${this.settings.milestoneCount}`);
 	}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		const data = await this.loadData();
+		if (data) {
+		this.settings = { ...DEFAULT_SETTINGS, ...data };
+		}
 	}
 
 	async saveSettings() {
@@ -91,44 +100,34 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+  class NoteProgressSettingsTab extends PluginSettingTab {
+	plugin: NoteProgressTrackerPlugin;
+  
+	constructor(app: App, plugin: NoteProgressTrackerPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
+		containerEl.createEl('h2', { text: 'Note Progress Tracker Settings' });
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+		.setName('Streak Count')
+		.setDesc('The number of consecutive days you have taken notes.')
+		.addText((text) => text.setValue(String(this.plugin.settings.streakCount)).onChange(async (value) => {
+			this.plugin.settings.streakCount = parseInt(value);
+			await this.plugin.saveSettings();
+		}));
+
+		new Setting(containerEl)
+		.setName('Badge Count')
+		.setDesc('The total number of badges you have earned.')
+		.addText((text) => text.setValue(String(this.plugin.settings.badgeCount)).onChange(async (value) => {
+			this.plugin.settings.badgeCount = parseInt(value);
+			await this.plugin.saveSettings();
+		}));
 	}
 }
